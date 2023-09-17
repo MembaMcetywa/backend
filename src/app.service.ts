24b9d4e4fab78ad4d22ab0bcab3569f853bf6643
Membathisi Mcetywa <iamemba10gmail.com>
2023-09-18 01:13:00 +0200

    feature(services): added validation service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    @Inject('VALIDATION') private readonly validationClient: ClientProxy,
+  ) {}
+
+  validatePhoneNumbers(phoneNumbers: string[]) {
+    try {
+      const client = this.validationClient.send<string[], string[]>(
+        { cmd: 'validate-phone-number' },
+        phoneNumbers,
+      );
+
+      return lastValueFrom(client);
+    } catch (error) {
+      throw new Error('Validation failed');
+    }
   }
 }
