19003de2190c705490dd41daaba1d13a84d72fef
Membathisi Mcetywa <iamemba10gmail.com>
2023-09-18 01:11:27 +0200

    feature(module): registered ClientsModule with injection token

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'VALIDATION',
+        transport: Transport.TCP,
+        options: {
+          port: 9000,
+          host: 'localhost',
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
