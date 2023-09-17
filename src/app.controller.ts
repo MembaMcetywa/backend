0793fda467598221cf312a8611a53787535f1139
Membathisi Mcetywa <iamemba10gmail.com>
2023-09-18 01:10:23 +0200

    feature(backend controller): added controller for validating phone numbers

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  HttpStatus,
+  HttpException,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('validate-phone-numbers')
+  validatePhoneNumbers(@Body() phoneNumbers: string[]) {
+    try {
+      const validationResults =
+        this.appService.validatePhoneNumbers(phoneNumbers);
+
+      return validationResults;
+    } catch (error) {
+      console.error('Validation failed:', error.message);
+      throw new HttpException('Validation failed', HttpStatus.BAD_REQUEST);
+    }
   }
 }
